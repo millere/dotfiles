2252b51d0c72563dda2985ccdd029c94ba054d1e
Isabelle Miller <izzy@stardust.fm>
2014-02-27 04:08:00 -0500

    Adding files helps

---
+++ cmus/lib.pl
@@ -0,0 +1,12 @@
+/home/exm/music/Von Shakes - Bohemia/01-Robinson Crusoe.flac
+/home/exm/music/Von Shakes - Bohemia/02-Talk n' Crawl.flac
+/home/exm/music/Von Shakes - Bohemia/03-My Side.flac
+/home/exm/music/Von Shakes - Bohemia/04-Control.flac
+/home/exm/music/Von Shakes - Bohemia/05-Bali II.flac
+/home/exm/music/Von Shakes - Bohemia/06-Your Own Holiday.flac
+/home/exm/music/Von Shakes - Bohemia/07-Pale.flac
+/home/exm/music/Von Shakes - Bohemia/08-Headaches.flac
+/home/exm/music/Von Shakes - Bohemia/09-Sit, Wait, and Pray.flac
+/home/exm/music/Von Shakes - Bohemia/10-Last Day on Earth.flac
+/home/exm/music/Von Shakes - Bohemia/11-That & This.flac
+/home/exm/music/Von Shakes - Bohemia/12-Away From Here.flac
